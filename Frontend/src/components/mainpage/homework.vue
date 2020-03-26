6bb25c539332f0188d5bd4233d473ae3c5c1b81e
ZhangShun <596602667@qq.com>
2020-03-26 22:34:30 +0800

    重构了一些代码

--- Frontend/src/components/mainpage/homework.vue
+++ Frontend/src/components/mainpage/homework.vue
@@ -1,6 +1,6 @@
 <template>
   <el-card>
-    <el-table :data="tableData"
+    <el-table :data="tableData2"
               @cell-click="contestclick"
               size="small">
       <el-table-column prop="id"
@@ -9,10 +9,14 @@
       <el-table-column prop="title"
                        label="作业"
                        :width="300"></el-table-column>
+      <el-table-column prop="acCount"
+                       label="已完成题目数量"></el-table-column>
+      <el-table-column prop="proCount"
+                       label="题目数量"></el-table-column>
       <el-table-column prop="begintime"
                        label="布置时间"></el-table-column>
-      <el-table-column prop="type"
-                       label="类型"></el-table-column>
+      <el-table-column prop="endtime"
+                       label="截止时间"></el-table-column>
       <el-table-column prop="classes"
                        label="班级"></el-table-column>
     </el-table>
@@ -21,17 +25,20 @@
 </template>
 
 <script>
+import moment from "moment";
 export default {
+
   data () {
     return {
       username: "",
       className: "",
-      currentpage: 1,
-      pagesize: 30,
-      totalcontest: 10,
+      totalcontest: 0,
       tableData: [],
       tableData2: [],
       loading: true,
+      contestIDs: [],
+      totProCount: [],
+      totAcCount: [],
       searchform: {
         type: "",
         title: ""
@@ -50,20 +57,57 @@ export default {
   created () {
     this.username = sessionStorage.username;
     this.className = this.$route.query.className;
-
     this.$axios
       .get("/contestinfo/?type=Homework&classes=" + this.className)
       .then(response => {
-
         this.tableData = response.data;
         this.totalcontest = response.data.length;
+        for (var i = 0; i < response.data.length; i++) {
+          this.contestIDs.push(response.data[i].id);
+          var bt = this.tableData[i].begintime;
+          bt = bt.split("T")
+          bt = bt[0];
+          this.$set(this.tableData[i], "begintime", bt);
+          bt = bt.split("-");
+          var et = new Date(bt[0], bt[1], bt[2]);
+          et.setSeconds(et.getDate() + response.data[i].lasttime);
+          et = moment(et).format("YYYY-MM-DD");
+          this.$set(this.tableData[i], "endtime", et);
+        }
         this.loading = false;
-      })
-      .catch(error => {
-        this.$message.error(
-          "服务器错误！" + "(" + JSON.stringify(error.response.data) + ")"
-        );
+      }).then(() => {
+        (async () => {
+          const dataArray = await Promise.all(
+            this.contestIDs.map(i => this.$axios.get(`/contestproblem/?contestid=${i}`))
+          );
+          for (const { data } of dataArray) {
+            if (typeof data === "object" && typeof data.length === "number") {
+              this.totProCount.push(JSON.stringify(data.length));
+            }
+          }
+          for (var ii = 0; ii < this.totalcontest; ii++) {
+            this.$set(this.tableData[ii], "proCount", this.totProCount[ii]);
+          }
+        })();
+      }).then(() => {
+        (async () => {
+          const dataArray2 = await Promise.all(
+            this.contestIDs.map(i => this.$axios.get(`/contestboard/?contestid=${i}&username=${this.username}&type=1`))
+          );
+          for (const { data2 } of dataArray2) {
+            if (typeof data2 === "object" && typeof data2.length === "number") {
+              this.totAcCount.push(JSON.stringify(data2.length));
+            }
+          }
+          for (var iii = 0; iii < this.totAcCount; iii++) {
+            this.$set(this.tableData[iii], "acCount", this.totAcCount[iii]);
+          }
+          this.tableData2 = this.tableData;
+          console.log(this.tableData2);
+        })();
       });
+
+
   }
 
 }
