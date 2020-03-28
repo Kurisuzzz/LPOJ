6dc7658462f989d8b1967ed07f875bb80cdd2d44
ZhangShun <596602667@qq.com>
2020-03-28 16:22:00 +0800

    更新作业统计

--- Frontend/src/components/mainpage/homework.vue
+++ Frontend/src/components/mainpage/homework.vue
@@ -32,13 +32,13 @@ export default {
     return {
       username: "",
       className: "",
-      totalcontest: 0,
+      totalcontest: 0,//总比赛数
       tableData: [],
       tableData2: [],
       loading: true,
-      contestIDs: [],
-      totProCount: [],
-      totAcCount: [],
+      contestIDs: [],//每场比赛的ID
+      totProCount: [],//每场比赛有多少题目
+      totAcCount: [],//每场比赛有多少AC题目
       searchform: {
         type: "",
         title: ""
@@ -62,6 +62,9 @@ export default {
       .then(response => {
         this.tableData = response.data;
         this.totalcontest = response.data.length;
+        for (var k = 0; k <= this.totalcontest; k++) {
+          this.totAcCount.push(0);
+        }
         for (var i = 0; i < response.data.length; i++) {
           this.contestIDs.push(response.data[i].id);
           var bt = this.tableData[i].begintime;
@@ -94,22 +97,25 @@ export default {
           const dataArray2 = await Promise.all(
             this.contestIDs.map(i => this.$axios.get(`/contestboard/?contestid=${i}&username=${this.username}&type=1`))
           );
-          for (const { data2 } of dataArray2) {
-            if (typeof data2 === "object" && typeof data2.length === "number") {
-              this.totAcCount.push(JSON.stringify(data2.length));
+          var lastrank;
+          var lastcontest;
+          if (dataArray2[0].data.length > 0) {
+            for (var data2 of dataArray2) {
+              for (var i = 0; i < data2.data.length; i++) {
+                if (data2.data[i].contestid == lastcontest && data2.data[i].problemrank == lastrank) continue;
+                this.totAcCount[data2.data[i].contestid] = parseInt(this.totAcCount[data2.data[i].contestid]) + parseInt(1);
+                lastrank = data2.data[i].problemrank;
+                lastcontest = data2.data[i].contestid;
+              }
+            }
+            for (var iii = 0; iii < this.totalcontest; iii++) {
+              this.$set(this.tableData[iii], "acCount", this.totAcCount[this.tableData[iii].id]);
             }
           }
-          for (var iii = 0; iii < this.totAcCount; iii++) {
-            this.$set(this.tableData[iii], "acCount", this.totAcCount[iii]);
-          }
           this.tableData2 = this.tableData;
-          console.log(this.tableData2);
         })();
       });
-
-
   }
-
 }
 </script>
 
